396e6fabce054f269e2f258e6f5ffd997a779c86
Rishabh Singh <rishabh.singh@rungta.org>
2024-03-04 23:06:52 +0530

    uifix0.1

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { IoArrowForward } from "react-icons/io5";
 
 function App() {
   return (
-    <div className=" h-screen ">
+    <div className=" h-screen w-full">
       <div className=" px-10 py-10 ">
-        <Home className=" h-screen ">
+        <Home className=" h-[90vh] ">
           <div>
             <NavBar />
             <div className="w-3/6 leading-8 px-14 py-6">
@@ -36,23 +36,60 @@ function App() {
                 />
               </div>
             </div>
-            <div className="absolute right-16 top-16">
-              <img src="YourOrg_Unit.png" alt="" />
+            <div className="absolute right-10 top-8">
+              <div className=" relative">
+                <div className="bottom-0 w-full rounded-bl-3xl rounded-br-3xl py-2 absolute text-white bg-[#0000005d] backdrop-blur-sm px-6">
+                  <h4 className=" text-4xl uppercase">
+                    Management & <br /> ORGANIZAtION
+                  </h4>
+                  <p className="py-6">
+                    Simplify administrative tasks to save time and resources.
+                    Make informed decisions with robust data analytics tools.
+                    Adopt scalable solutions for sustainable institutional
+                    growth.
+                  </p>
+                  <div className=" flex justify-between w-[300px] cursor-pointer border ps-4 pr-1 py-1 rounded-lg">
+                    <button>Click to know more</button>
+                    <MdOutlineArrowOutward
+                      className="bg-red-700 rounded-md text-white "
+                      size={20}
+                    />
+                  </div>
+                </div>
+                <img src="YourOrg_Unit.png" alt="" />
+              </div>
             </div>
-            <div className="absolute top-[29.6rem] right-[38.5rem]">
-              <img src="YourTem_Unit.png" alt="" />
+            <div className="absolute bottom-[14px] right-[38rem]">
+              <div className=" relative">
+                <p className=" absolute text-xl ms-4 mt-2">
+                  ADMINS &<br /> FINANCE TEAM
+                </p>
+                <img src="YourTem_Unit.png" alt="" />
+              </div>
             </div>
-            <div className="absolute top-[33rem] right-[57.8rem]">
-              <img src="/Group 73.png" alt="" />
+            <div>
+              <div className="absolute bottom-[14px] right-[58rem]">
+                <div className=" relative">
+                  <p className=" absolute text-xl ms-4 mt-2">
+                    TEACHERS <br />& STAFFS
+                  </p>
+                  <img src="/Group 73.png" alt="" />
+                </div>
+              </div>
             </div>
-            <div className="absolute top-[35rem] right-[74.2rem]">
-              <img src="/Your Student Unit.png" alt="" />
+            <div className="absolute bottom-[14px] left-[32px]">
+              <div className=" relative">
+                <p className=" absolute text-xl ms-4 mt-2">
+                  STUDENTS <br />& PARENTS
+                </p>
+                <img src="/Your Student Unit.png" alt="" />
+              </div>
             </div>
 
-            <button className="bg-red-700 rounded-xl font-thin py-1 text-white px-5 absolute right-[4.3rem] top-[3.9rem]">
+            <button className="bg-red-700 rounded-xl font-thin py-1 text-white px-5 absolute right-[3rem] top-[32px]">
               Contact Us{" "}
             </button>
-            <button className="flex items-center gap-x-1 bg-red-700 rounded-xl  py-1 text-white px-3 absolute right-[4.2rem] top-[43.5rem]">
+            <button className="flex items-center gap-x-1 border-4 bg-red-700 rounded-xl  py-1 text-white px-3 absolute right-[2.6rem] bottom-[15px]">
               Book Free Demo{" "}
               <IoArrowForward
                 className="text-black bg-white rounded-full "
